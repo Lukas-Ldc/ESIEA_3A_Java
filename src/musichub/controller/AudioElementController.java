5d7bf5cf35abd00d1911827be9c2ef69a873e7e3
celia <celia@LAPTOP-L1PBJO32>
2021-06-28 18:40:39 +0200

    add function to be able to listen to a song

--- src/musichub/controller/AudioElementController.java
+++ src/musichub/controller/AudioElementController.java
@@ -1,5 +1,90 @@
 package musichub.controller;
 
-public class AudioElementController {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JOptionPane;
+
+
+public class AudioElementController {
+	
+	private String root = System.getProperty("user.dir");
+	private Clip clip = null;
+	private long timeStopPos = 0;
+	private Thread currentActiveThread = null;
+	
+	public AudioElementController() {
+		
+		try {
+			clip = AudioSystem.getClip();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void play(String songName) {
+		
+		String path = root + "/audioFile/" + songName + ".wav";
+		File file = new File(path);
+		timeStopPos = 0;
+		
+		try {
+			
+			if(file.exists()) {
+
+				AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+				clip.open(audioStream);
+				clip.start();
+				
+//				// need an active thread to be able to read the audio file
+//				JOptionPane.showMessageDialog(null, "Click OK to stop music");
+//				Thread.sleep(clip.getMicrosecondLength()/1000);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void stop() {
+		timeStopPos = clip.getMicrosecondPosition();
+		clip.stop();
+	}
+	
+	public void continueAudio() {
+		clip.setMicrosecondPosition(timeStopPos);
+		clip.start();
+		JOptionPane.showMessageDialog(null, "Click OK to stop music");
+	}
+	
+	public void playSingleSong(String songName) {
+		play(songName);
+		JOptionPane.showMessageDialog(null, "Click OK to stop music");
+	}
+	
+	public void playLoop(String songName) {
+		play(songName);
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+		JOptionPane.showMessageDialog(null, "Click OK to stop music");
+	}
+	
 }
